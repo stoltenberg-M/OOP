c25c7eb08e394d9421bdbdeccb81e95f688ad30f
stoltenberg <stoltenbergmagnus@icloud.com>
2025-03-24 14:57:47 +0100

    idk jeg har lavet noget meger

--- Øvelser_til_OOP.js
+++ Øvelser_til_OOP.js
@@ -3,12 +3,16 @@
 function setup(){
 createCanvas(400, 400);
 background(180,200,180); 
+
+A1 = new kage()
+A1.O
 }
 
 class kage{
     constructor(){
         this.x=100
         this.y=300
+        this.O=rect(50,50,10,10)
     }
 }
 
