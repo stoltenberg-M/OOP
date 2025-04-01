9152cd21b06a1f8746fc5cf5ef9d054c156b0395
FrederikSJ <frederikjohansen7@gmail.com>
2025-04-01 10:44:39 +0200

    dotProdukt

--- Øvelser_til_OOP.js
+++ Øvelser_til_OOP.js
@@ -1,8 +1,11 @@
 let v1,v2
+
  
 function setup() {
   createCanvas(400, 400);
   v1 = new Vector(100,100,20,50)
+  v2 = new Vector(200,200,20,50)
+  v1.dotProdukt(v2)
 }
  
 function draw() {
@@ -23,5 +26,10 @@ class Vector{
     line(this.x0,this.y0,this.x1,this.y1)
     circle(this.x1,this.y1,10)
   }
+dotProdukt(v2){
+  return (thisx*v2x+thisy*v2y)
+}
 
+
+  //['scalar(k)'] 
 }
