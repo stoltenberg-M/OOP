3eba5b8bb2056f0ac80f1890ded0e55e6fcdbb8d
stoltenberg <stoltenbergmagnus@icloud.com>
2025-05-24 16:52:07 +0200

    no to data

--- øvelserne_ig/ctrl_C.js
+++ øvelserne_ig/ctrl_C.js
@@ -54,3 +54,32 @@ function draw() {
     tR = color(random(255), random(255), random(255));
   }
 }
+/*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*/
+let x1, y1;
+let xS1, yS1;
+let tR1;
+
+
+function setup() {
+  createCanvas(750, 500);
+  x1 = 20;
+  y1 = 250;
+  xS1 = 6.5;
+  yS1 = 0.5 * xS1;
+  tR = color(255, 100, 50);
+}
+
+function draw() {
+  background(tR1);
+  y1 += yS1;
+  x1 += xS1;
+
+  fill(255, 0, 0);
+  circle(x1, y1, 30);
+  if (y1 > height-15 || y1 < 15) {
+    yS1 = -yS1;
+  }
+  if (x1 > width - 15 || x1 < 15) {
+    xS1 = -xS1;
+}
+}
