400357c83c07f40b8cf6d36f658ef30d0e8d63ba
stoltenberg <stoltenbergmagnus@icloud.com>
2025-05-24 18:05:00 +0200

    fish go boom

--- fish game/fishGame.js
+++ fish game/fishGame.js
@@ -1,16 +1,18 @@
-let x;
-let y;
+let x=100,y=100;
+let F1,Sark1;
+let xS=400,yS=400,speed,distance,d1,d2;
 
 function setup(){
 createCanvas(1675,800);
 background(220);
-
+F1 = new Fish(x, y)
+Sark1 = new shark(xS, yS);
 }
 
 class Fish{
     constructor(x,y){
-        this.x=x || 100
-        this.y=y || 100
+        this.x=x 
+        this.y=y 
     } 
     show(col){
         fill(col)
@@ -21,44 +23,60 @@ class Fish{
         ellipse(this.x, this.y + 6, 15, 5); // food hold
     }
 }
-
-
-function draw(){
-    youFish_move();
-    fishMan();
+class shark{
+    constructor(x,y){
+        this.x=x 
+        this.y=y 
+    } 
+    show(col){
+        fill(col)
+        ellipse(this.x, this.y, 40, 40)
+        fill(0);
+        ellipse(this.x + 10, this.y - 5, 5, 5); // Eye
+        ellipse(this.x - 10, this.y - 5, 5, 5); // Eye
+        ellipse(this.x, this.y + 6, 15, 5); // food hold
+    }
 }
 
-function youFish_move() {
-    if (x <= 1) {
-    x;
-  }
-  else if (keyIsDown(65)) {
+function draw(){
+    background(220);
+    Fish_move();
+
+    F1.x = x; F1.y = y;
+        F1.show("red")
+    Sark1.x = xS; Sark1.y = yS;
+        Sark1.show("blue")
+}
+
+function Fish_move() {
+  if (keyIsDown(65) && x > 15) {
     x -= 5;
   }
 
-  if (x >= 510) {
+  if (x >= 1675) {
     x;
   }
-  else if (keyIsDown(68)) {
+  if (keyIsDown(68) && x < 1660) {
     x += 5;
   }
   
-  if (y <= 1) {
-    y;
-  }
-  else if (keyIsDown(87)) {
+  if (keyIsDown(87) && y > 15) {
     y -= 5;
   }
-  if (y >= 510) {
-    y;
-  }
-  else if (keyIsDown(83)) {
+
+  if (keyIsDown(83) && y < 785) {
     y += 5;
+  }
+
+//shark
+  d1 = x - xS; d2 = y - yS;
+  distance = dist(x,y,xS,yS);
+  speed = 3
+
+
+  if (distance > 1) {
+    xS += (d1/distance)*speed;
+    yS += (d2/distance)*speed;
   }
 }
 
-function fishMan() {
-    let f1 = new Fish(x, y)
-    f1.show("red")
-    console.log(x,y);
-}
