700090157cecc770eabb321c2b85386c99e317ef
FrederikSJ <frederikjohansen7@gmail.com>
2025-05-25 14:36:54 +0200

    fish move like fish

--- fish game/fishGame.js
+++ fish game/fishGame.js
@@ -1,54 +1,58 @@
-let x=100,y=100;
-let F1,Sark1;
-let xS=400,yS=400,speed,distance,d1,d2;
+let x = 100, y = 100;
+let F1, Sark1;
+let xS = 400, yS = 400, speed, distance, d1, d2;
 let Water_grass = [];
 let saeImage;
+
+let fishAngle = 0;
+let noiseOffset = 0;
 
 function preload() {
     saeImage = loadImage("sae baed.png"); 
 }
 
 function setup(){
-  createCanvas(1675,800);
-  background(saeImage);
-  F1 = new myFish(x, y)
-  Sark1 = new shark(xS, yS);
+    createCanvas(1675, 800);
+    background(saeImage);
+    F1 = new myFish(x, y);
+    Sark1 = new shark(xS, yS);
 
-  for (let i = 0; i < width; i += 10){
-      Water_grass.push(new Water_weed(i, 800));
-  }
-}
-
-class myFish{
-    constructor(x,y){
-        this.x=x 
-        this.y=y 
-    } 
-    show(col){
-        fill(col)
-        ellipse(this.x, this.y, 30, 30)
-        fill(0);
-        ellipse(this.x + 10, this.y - 5, 5, 5); // Eye
-        ellipse(this.x - 10, this.y - 5, 5, 5); // Eye
-        ellipse(this.x, this.y + 6, 15, 5); // food hold
-    }
-}
-class shark{
-    constructor(x,y){
-        this.x=x 
-        this.y=y 
-    } 
-    show(col){
-        fill(col)
-        ellipse(this.x, this.y, 40, 40)
-        fill(0);
-        ellipse(this.x + 10, this.y - 5, 5, 5); // Eye
-        ellipse(this.x - 10, this.y - 5, 5, 5); // Eye
-        ellipse(this.x, this.y + 6, 15, 5); // food hold
+    for (let i = 0; i < width; i += 10){
+        Water_grass.push(new Water_weed(i, 800));
     }
 }
 
-class Water_weed{
+class myFish {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    } 
+    show(col){
+        fill(col);
+        ellipse(this.x, this.y, 30, 30);
+        fill(0);
+        ellipse(this.x + 10, this.y - 5, 5, 5); // Eye
+        ellipse(this.x - 10, this.y - 5, 5, 5); // Eye
+        ellipse(this.x, this.y + 6, 15, 5); // Mouth
+    }
+}
+
+class shark {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    } 
+    show(col){
+        fill(col);
+        ellipse(this.x, this.y, 40, 40);
+        fill(0);
+        ellipse(this.x + 10, this.y - 5, 5, 5); // Eye
+        ellipse(this.x - 10, this.y - 5, 5, 5); // Eye
+        ellipse(this.x, this.y + 6, 15, 5); // Mouth
+    }
+}
+
+class Water_weed {
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -60,7 +64,7 @@ class Water_weed{
         let sharkDist = dist(this.x, this.y, Sark1.x, Sark1.y);
 
         if (fishDist < 60 || sharkDist < 80) {
-            let closestX = (fishDist < sharkDist) ? x : xS;
+            let closestX = (fishDist < sharkDist) ? F1.x : Sark1.x;
             this.targetAngle = map(this.x - closestX, -50, 50, -PI / 4, PI / 4);
         } else {
             this.targetAngle = 0;
@@ -91,39 +95,41 @@ function draw(){
 }
 
 function Fish_move() {
-  if (keyIsDown(65) && x > 15) {
-    x -= 5;
-  }
+    // Natural fish motion using Perlin noise
+    noiseOffset += 0.01;
+    let angleChange = map(noise(noiseOffset), 0, 1, -0.1, 0.1);
+    fishAngle += angleChange;
 
-  if (x >= 1675) {
-    x;
-  }
-  if (keyIsDown(68) && x < 1660) {
-    x += 5;
-  }
-  
-  if (keyIsDown(87) && y > 15) {
-    y -= 5;
-  }
+    let stepSize = 2.5;
+    x += cos(fishAngle) * stepSize;
+    y += sin(fishAngle) * stepSize;
 
-  if (keyIsDown(83) && y < 785) {
-    y += 5;
-  }
+    // Keep fish inside canvas
+    if (x < 15 || x > width - 15) {
+        fishAngle = PI - fishAngle;
+    }
+    if (y < 15 || y > height - 15) {
+        fishAngle = -fishAngle;
+    }
 
-  //shark
-  d1 = x - xS; d2 = y - yS;
-  distance = dist(x,y,xS,yS);
-  speed = 4
+    // Shark chases fish
+    d1 = x - xS;
+    d2 = y - yS;
+    distance = dist(x, y, xS, yS);
+    speed = 4;
 
-  if (distance > 1) {
-    xS += (d1/distance)*speed;
-    yS += (d2/distance)*speed;
-  }
+    if (distance > 1) {
+        xS += (d1 / distance) * speed;
+        yS += (d2 / distance) * speed;
+    }
 }
 
 function allFish_go() {
-    F1.x = x; F1.y = y;
-    F1.show("red")
-    Sark1.x = xS; Sark1.y = yS;
-    Sark1.show("blue")
+    F1.x = x;
+    F1.y = y;
+    F1.show("red");
+
+    Sark1.x = xS;
+    Sark1.y = yS;
+    Sark1.show("blue");
 }
