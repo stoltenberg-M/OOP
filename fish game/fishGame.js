7f05e35cdeb68da7cd6183b0bd275cffce91f0a7
FrederikSJ <frederikjohansen7@gmail.com>
2025-05-25 15:25:42 +0200

    added flere fisk og nedarv

--- fish game/fishGame.js
+++ fish game/fishGame.js
@@ -1,38 +1,42 @@
-let x = 100, y = 100;
-let F1, Sark1;
-let xS = 400, yS = 400;
-let Water_grass = [];
 let saeImage;
-
-let fishAngle = 0;
-let fishNoiseOffset = 0;
-
+let allFish = [];
+let shark;
+let xS = 400, yS = 400;
 let sharkAngle = 0;
 let sharkNoiseOffset = 1000;
 let sharkMode = "patrol";
 let sharkTimer = 0;
 
+let Water_grass = [];
+
 function preload() {
     saeImage = loadImage("sae baed.png"); 
 }
 
-function setup(){
+function setup() {
     createCanvas(1675, 800);
     background(saeImage);
-    F1 = new myFish(x, y);
-    Sark1 = new shark(xS, yS);
+
+    allFish.push(new MyFish(100, 100)); // styret fisk
+
+    for (let i = 0; i < 5; i++) {
+        allFish.push(new AIFish(random(200, 1600), random(100, 700), i * 300));
+    }
+
+    shark = new Shark(xS, yS);
 
     for (let i = 0; i < width; i += 10){
-        Water_grass.push(new Water_weed(i, 800));
+        Water_grass.push(new WaterWeed(i, 800));
     }
 }
 
-class myFish {
-    constructor(x, y){
+class Fish {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-    } 
-    show(col){
+    }
+
+    show(col = "red") {
         fill(col);
         ellipse(this.x, this.y, 30, 30);
         fill(0);
@@ -40,36 +44,122 @@ class myFish {
         ellipse(this.x - 10, this.y - 5, 5, 5);
         ellipse(this.x, this.y + 6, 15, 5);
     }
+
+    move() {
+        // Skal overskrives af subklasser
+    }
 }
 
-class shark {
-    constructor(x, y){
+class MyFish extends Fish {
+    constructor(x, y) {
+        super(x, y);
+        this.angle = 0;
+        this.noiseOffset = 0;
+    }
+
+    move() {
+        this.noiseOffset += 0.01;
+        let angleChange = map(noise(this.noiseOffset), 0, 1, -0.1, 0.1);
+        this.angle += angleChange;
+
+        this.x += cos(this.angle) * 2.5;
+        this.y += sin(this.angle) * 2.5;
+
+        if (this.x < 15 || this.x > width - 15) this.angle = PI - this.angle;
+        if (this.y < 15 || this.y > height - 15) this.angle = -this.angle;
+    }
+
+    show() {
+        super.show("red");
+    }
+}
+
+class AIFish extends Fish {
+    constructor(x, y, noiseOffset) {
+        super(x, y);
+        this.noiseOffset = noiseOffset;
+        this.angle = random(TWO_PI);
+    }
+
+    move() {
+        this.noiseOffset += 0.01;
+        let angleChange = map(noise(this.noiseOffset), 0, 1, -0.1, 0.1);
+        this.angle += angleChange;
+
+        this.x += cos(this.angle) * 2;
+        this.y += sin(this.angle) * 2;
+
+        if (this.x < 15 || this.x > width - 15) this.angle = PI - this.angle;
+        if (this.y < 15 || this.y > height - 15) this.angle = -this.angle;
+    }
+
+    show() {
+        super.show("orange");
+    }
+}
+
+class Shark {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-    } 
-    show(col){
-        fill(col);
+    }
+
+    show() {
+        fill("blue");
         ellipse(this.x, this.y, 40, 40);
         fill(0);
         ellipse(this.x + 10, this.y - 5, 5, 5);
         ellipse(this.x - 10, this.y - 5, 5, 5);
         ellipse(this.x, this.y + 6, 15, 5);
     }
+
+    move(targetX, targetY) {
+        let detectionRange = 150;
+        let now = millis();
+
+        if (sharkMode === "patrol" && dist(targetX, targetY, this.x, this.y) < detectionRange) {
+            sharkMode = "chase";
+            sharkTimer = now;
+        } else if (sharkMode === "chase" && now - sharkTimer > 5000) {
+            sharkMode = "ignore";
+            sharkTimer = now;
+        } else if (sharkMode === "ignore" && now - sharkTimer > 5000) {
+            sharkMode = "patrol";
+        }
+
+        if (sharkMode === "patrol") {
+            sharkNoiseOffset += 0.008;
+            let angleChange = map(noise(sharkNoiseOffset), 0, 1, -0.05, 0.05);
+            sharkAngle += angleChange;
+
+            this.x += cos(sharkAngle) * 2;
+            this.y += sin(sharkAngle) * 2;
+        } else if (sharkMode === "chase") {
+            let dx = targetX - this.x;
+            let dy = targetY - this.y;
+            let distToTarget = dist(targetX, targetY, this.x, this.y);
+            if (distToTarget > 1) {
+                this.x += (dx / distToTarget) * 3.5;
+                this.y += (dy / distToTarget) * 3.5;
+            }
+        }
+    }
 }
 
-class Water_weed {
+class WaterWeed {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.angle = 0;
         this.targetAngle = 0;
     }
-    update() {
-        let fishDist = dist(this.x, this.y, F1.x, F1.y);
-        let sharkDist = dist(this.x, this.y, Sark1.x, Sark1.y);
+
+    update(fishX, fishY, sharkX, sharkY) {
+        let fishDist = dist(this.x, this.y, fishX, fishY);
+        let sharkDist = dist(this.x, this.y, sharkX, sharkY);
 
         if (fishDist < 60 || sharkDist < 80) {
-            let closestX = (fishDist < sharkDist) ? F1.x : Sark1.x;
+            let closestX = (fishDist < sharkDist) ? fishX : sharkX;
             this.targetAngle = map(this.x - closestX, -50, 50, -PI / 4, PI / 4);
         } else {
             this.targetAngle = 0;
@@ -77,6 +167,7 @@ class Water_weed {
 
         this.angle = lerp(this.angle, this.targetAngle, 0.1);
     }
+
     show() {
         push();
         translate(this.x, this.y);
@@ -88,87 +179,20 @@ class Water_weed {
     }
 }
 
-function draw(){
-    background(saeImage); 
-    Fish_move();
-    Shark_move();
-    allFish_go();
+function draw() {
+    background(saeImage);
 
-    for (let i of Water_grass) {
-        i.update();
-        i.show();
-    }
-}
-
-function Fish_move() {
-    fishNoiseOffset += 0.01;
-    let angleChange = map(noise(fishNoiseOffset), 0, 1, -0.1, 0.1);
-    fishAngle += angleChange;
-
-    let stepSize = 2.5;
-    x += cos(fishAngle) * stepSize;
-    y += sin(fishAngle) * stepSize;
-
-    if (x < 15 || x > width - 15) {
-        fishAngle = PI - fishAngle;
-    }
-    if (y < 15 || y > height - 15) {
-        fishAngle = -fishAngle;
-    }
-}
-
-function Shark_move() {
-    let detectionRange = 150;
-    let now = millis();
-
-    // Handle state transitions
-    if (sharkMode === "patrol" && dist(x, y, xS, yS) < detectionRange) {
-        sharkMode = "chase";
-        sharkTimer = now;
-    } else if (sharkMode === "chase" && now - sharkTimer > 5000) {
-        sharkMode = "ignore";
-        sharkTimer = now;
-    } else if (sharkMode === "ignore" && now - sharkTimer > 5000) {
-        sharkMode = "patrol";
+    for (let fish of allFish) {
+        fish.move();
+        fish.show();
     }
 
-    if (sharkMode === "patrol") {
-        // Natural patrol movement
-        sharkNoiseOffset += 0.008;
-        let angleChange = map(noise(sharkNoiseOffset), 0, 1, -0.05, 0.05);
-        sharkAngle += angleChange;
+    let targetFish = allFish[0];
+    shark.move(targetFish.x, targetFish.y);
+    shark.show();
 
-        let stepSize = 2;
-        xS += cos(sharkAngle) * stepSize;
-        yS += sin(sharkAngle) * stepSize;
-
-        if (xS < 15 || xS > width - 15) {
-            sharkAngle = PI - sharkAngle;
-        }
-        if (yS < 15 || yS > height - 15) {
-            sharkAngle = -sharkAngle;
-        }
+    for (let weed of Water_grass) {
+        weed.update(targetFish.x, targetFish.y, shark.x, shark.y);
+        weed.show();
     }
-
-    if (sharkMode === "chase") {
-        let d1 = x - xS;
-        let d2 = y - yS;
-        let distance = dist(x, y, xS, yS);
-        let speed = 3.5;
-
-        if (distance > 1) {
-            xS += (d1 / distance) * speed;
-            yS += (d2 / distance) * speed;
-        }
-    }
-}
-
-function allFish_go() {
-    F1.x = x;
-    F1.y = y;
-    F1.show("red");
-
-    Sark1.x = xS;
-    Sark1.y = yS;
-    Sark1.show("blue");
 }
